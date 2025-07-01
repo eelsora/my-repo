2c5e79bca3fcfd53c06e693c3037eac8bf5363ca
sora <eelossra@gmail.com>
2025-07-01 23:26:03 +0900

    ACL을 적용하여 전략계층으로 활용할 수 있도록함

---
+++ vite-project/weather-app/src/models/SearchResultItemType.ts
@@ -0,0 +1,47 @@
+import type { RemoteSearchResultItem } from "./RemoteSearchResultItem";
+
+const countryMap = {
+  AU: "Australia",
+  US: "United States",
+  GB: "United Kingdom",
+};
+
+class SearchResultItemType {
+  private readonly _city: string;
+  private readonly _state: string;
+  private readonly _country: string;
+  private readonly _lat: number;
+  private readonly _long: number;
+
+  constructor(item: RemoteSearchResultItem) {
+    this._city = item.name;
+    this._state = item.state;
+    this._country = item.country;
+    this._lat = item.lat;
+    this._long = item.lon;
+  }
+
+  get city() {
+    return this._city;
+  }
+
+  get state() {
+    return this._state;
+  }
+
+  get country() {
+    return (
+      countryMap[this._country as keyof typeof countryMap] || this._country
+    );
+  }
+
+  get latitude() {
+    return this._lat;
+  }
+
+  get longitude() {
+    return this._long;
+  }
+}
+
+export { SearchResultItemType };
