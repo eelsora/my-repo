f35f75d138e1d0ef4ae759b1aa68b1b987621691
sora <eelossra@gmail.com>
2025-07-02 22:46:51 +0900

    feat: matcher 사용을 위한 설정 추가

--- vite-project/weather-app/vite.config.ts
+++ vite-project/weather-app/vite.config.ts
@@ -8,5 +8,9 @@ export default defineConfig({
   // server: {
   //   port: 3000,
   // },
-  test: { globals: true, environment: "happy-dom" },
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./setup.ts"],
+  },
 });
